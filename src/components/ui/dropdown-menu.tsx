44b1549478f07ff41ccf97eed7ab4db3d80da964
asfalfa <glkin9@gmail.com>
2024-07-25 16:05:15 +0200

    style: :lipstick: Adjust styling of dropdown menu items on hover

--- src/components/ui/dropdown-menu.tsx
+++ src/components/ui/dropdown-menu.tsx
@@ -81,7 +81,7 @@ const DropdownMenuItem = React.forwardRef<
   <DropdownMenuPrimitive.Item
     ref={ref}
     className={cn(
-      "hover:bg-neutral-200 dark:hover:bg-neutral-800 relative flex cursor-default select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none transition-colors focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50",
+      "z-10 hover:bg-neutral-200 dark:hover:bg-neutral-800 relative flex cursor-default select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none transition-colors focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50",
       inset && "pl-8",
       className
     )}
