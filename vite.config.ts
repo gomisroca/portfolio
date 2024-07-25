bf9f9a5aa0ab342edca2a2da584ba73ac1b7c756
asfalfa <glkin9@gmail.com>
2024-07-25 16:22:30 +0200

    ci: :rocket: Workflow to deploy to GH Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/portfolio/",
   plugins: [react()],
   resolve: {
     alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
