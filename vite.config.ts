ed8f5d944b17ba5b2ac5da1dfe42faa401aa39bb
asfalfa <glkin9@gmail.com>
2024-07-25 18:32:42 +0200

    feat: :ambulance: added deploy and predeploy structure

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,8 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/portfolio/",
   plugins: [react()],
+  base: "/portfolio/",
   resolve: {
     alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
   },
