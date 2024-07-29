385eb3f4463c2269eb1aea46b91d70a412c592d4
asfalfa <glkin9@gmail.com>
2024-07-29 16:26:51 +0200

    fix: :bug: Navbar flying off on flipword trigger

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,7 +5,7 @@ import {Menubar, MenubarMenu} from '@/components/ui/menubar'
 function Navbar() {
   return (
     <div>
-      <Menubar className="backdrop-blur-md flex border-none z-10 md:mx-1 lg:mx-2 my-1 md:my-2 justify-end fixed top-0 right-0">
+      <Menubar className="backdrop-blur-md flex border-none z-10 md:mx-1 lg:mx-2 my-1 md:my-2 justify-end absolute top-0 right-0">
         <MenubarMenu>
           <ContactMenu />
           <ModeMenu />
