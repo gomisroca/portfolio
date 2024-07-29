4e972378bf8d7035556d39283ec410896f221c78
asfalfa <glkin9@gmail.com>
2024-07-29 16:10:08 +0200

    feat: :sparkles: Add Github link to portfolio repo in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,16 @@
+import { Link } from "react-router-dom"
+
 function Footer() {
   return (
-    <div className="font-mw flex z-10 mx-6 my-2 justify-end bottom-0 right-0">
-      Updated July 2024
+    <div className="flex z-10 mx-6 py-2 justify-end bottom-0 right-0 gap-x-2 items-center">
+      <span>Updated July 2024</span>
+      <Link to="https://github.com/gomisroca/portfolio">
+        <img
+          alt="Github logo"
+          src="./github-mark.svg"
+          className="invert-0 dark:invert h-[20px]"
+        />
+      </Link>
     </div>
   )
 }
