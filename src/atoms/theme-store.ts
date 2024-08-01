1518adca6e581f78700184e39efa0125c2a509d9
asfalfa <glkin9@gmail.com>
2024-08-01 09:37:42 +0200

    Add store for jotai

---
+++ src/atoms/theme-store.ts
@@ -0,0 +1,11 @@
+import { createStore } from "jotai";
+import themeAtom from "@/atoms/theme-atom";
+
+const themeStore = createStore();
+
+themeStore.set(
+  themeAtom,
+  localStorage.getItem("portfolio-ui-theme") || "system"
+);
+
+export default themeStore;
