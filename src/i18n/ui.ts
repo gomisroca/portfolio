9c55e622d566e73cdf5e6ee09e917430c86fa9db
asfalfa <glkin9@gmail.com>
2024-11-27 15:14:41 +0100

    chore: add translations

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -6,24 +6,59 @@ export const languages = {
 };
 
 export const defaultLang = "en";
-
 export const showDefaultLang = false;
 
 export const ui = {
   en: {
     "nav.home": "Home",
     "nav.about": "About",
+    "nav.contact": "Contact",
+
+    "footer.theme": "Theme",
+    "footer.en": "English",
+    "footer.es": "Spanish",
+    "footer.cat": "Catalan",
+    "footer.de": "German",
+
+    "landing.welcome": "Welcome to my portfolio",
   },
   es: {
     "nav.home": "Inicio",
     "nav.about": "Sobre Mi",
+    "nav.contact": "Contacto",
+
+    "footer.theme": "Tema",
+    "footer.en": "Inglés",
+    "footer.es": "Español",
+    "footer.cat": "Catalán",
+    "footer.de": "Alemán",
+
+    "landing.welcome": "Bienvenido a mi portafolio",
   },
   cat: {
     "nav.home": "Inici",
     "nav.about": "Sobre Mi",
+    "nav.contact": "Contacte",
+
+    "footer.theme": "Tema",
+    "footer.en": "Anglès",
+    "footer.es": "Espanyol",
+    "footer.cat": "Català",
+    "footer.de": "Alemany",
+
+    "landing.welcome": "Benvingut al meu portafolis",
   },
   de: {
     "nav.home": "Anfang",
     "nav.about": "Über Mich",
+    "nav.contact": "Kontakt",
+
+    "footer.theme": "Thema",
+    "footer.en": "Englisch",
+    "footer.es": "Spanisch",
+    "footer.cat": "Katalanisch",
+    "footer.de": "Deutsch",
+
+    "landing.welcome": "Willkommen auf mein Portfolio",
   },
 } as const;
