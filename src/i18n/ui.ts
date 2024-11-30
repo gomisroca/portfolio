1af0a02667c3cd6fb9ed628a52353c5fb58ff082
asfalfa <glkin9@gmail.com>
2024-11-30 15:34:16 +0100

    feat: decouple i18n translations

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -8,15 +8,8 @@ export const languages = {
 export const defaultLang = "en";
 export const showDefaultLang = false;
 
-export const ui = {
+const ui = {
   en: {
-    landing: {
-      welcome: [
-        `<h1 class="text-4xl font-bold">Hi, I'm Adrià 👋</h1>`,
-        `<h3 class="text-2xl font-semibold">Full-stack web developer</h3>`,
-        `<p>Specialized in the React ecosystem, with a focus on building web applications that are <span class="font-semibold">powerful</span>, <span class="font-semibold">user-friendly</span>, and <span class="font-semibold">highly performant</span>.</p>`,
-      ],
-    },
     nav: {
       home: "Home",
       about: "About",
@@ -24,13 +17,6 @@ export const ui = {
     },
   },
   es: {
-    landing: {
-      welcome: [
-        "Hola, soy Adrià 👋.",
-        "Soy un desarrollador web full-stack.",
-        "Aplicaciones web limpias, potentes y eficientes son mi pasión.",
-      ],
-    },
     nav: {
       home: "Inicio",
       about: "Sobre Mi",
@@ -38,13 +24,6 @@ export const ui = {
     },
   },
   cat: {
-    landing: {
-      welcome: [
-        "Hola, soy Adrià 👋.",
-        "Soy un desarrollador web full-stack.",
-        "Aplicaciones web limpias, potentes y eficientes son mi pasión.",
-      ],
-    },
     nav: {
       home: "Inici",
       about: "Sobre Mi",
@@ -52,17 +31,12 @@ export const ui = {
     },
   },
   de: {
-    landing: {
-      welcome: [
-        "Hola, soy Adrià 👋.",
-        "Soy un desarrollador web full-stack.",
-        "Aplicaciones web limpias, potentes y eficientes son mi pasión.",
-      ],
-    },
     nav: {
       home: "Anfang",
       about: "Über Mich",
       contact: "Kontakt",
     },
   },
-} as const;
+};
+
+export default ui;
