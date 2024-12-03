02242a20d3a411aa58ea7ba666b118a2f75d9655
asfalfa <glkin9@gmail.com>
2024-12-03 12:39:43 +0100

    feat: update blog schema and add dummy entries

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,11 +1,22 @@
-import { defineCollection, z } from "astro:content";
+import { defineCollection, reference, z } from "astro:content";
 
 const blogCollection = defineCollection({
+  type: "content",
   schema: z.object({
-    title: z.string(),
-    author: z.string(),
+    title: z
+      .string()
+      .max(100, { message: "Title must be 100 characters or less" }),
+    description: z
+      .string()
+      .max(200, { message: "Title must be 200 characters or less" }),
     date: z.date(),
-    content: z.string(),
+    headerImage: z
+      .object({
+        src: z.string(),
+        alt: z.string(),
+      })
+      .optional(),
+    relatedPosts: z.array(reference("blog")).optional(),
   }),
 });
 
