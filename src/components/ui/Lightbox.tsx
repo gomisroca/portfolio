e15a4f056d7fe6fbf8442fe2b8d7b1a226e99cd6
asfalfa <glkin9@gmail.com>
2024-12-05 16:00:19 +0100

    feat: add lightbox component based on radix dialog

---
+++ src/components/ui/Lightbox.tsx
@@ -0,0 +1,31 @@
+import { MousePointerClick } from "lucide-react";
+import { Dialog, DialogContent, DialogTrigger } from "./dialog";
+import { motion } from "framer-motion";
+
+function Lightbox({ children }: { children: React.ReactNode }) {
+  return (
+    <Dialog>
+      <DialogTrigger>
+        <motion.span
+          animate={{
+            scale: 1.1,
+            rotateZ: 10,
+            transition: {
+              duration: 2,
+              ease: "easeInOut",
+              repeat: Infinity,
+              repeatType: "mirror",
+            },
+          }}
+          className="absolute right-2 top-2 rounded-full bg-zinc-200 p-1 shadow-md dark:bg-zinc-800"
+        >
+          <MousePointerClick className="h-6 w-6 text-zinc-800 dark:text-zinc-200" />
+        </motion.span>
+        {children}
+      </DialogTrigger>
+      <DialogContent className="rounded-md">{children}</DialogContent>
+    </Dialog>
+  );
+}
+
+export default Lightbox;
