dcec06556e06e538ec12a9764067e1b43781578b
asfalfa <glkin9@gmail.com>
2024-12-06 13:42:44 +0100

    feat: add technology type

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,12 @@
 /// <reference path="../.astro/types.d.ts" />
+
+type Technology =
+  | "Next.js"
+  | "React"
+  | "TailwindCSS"
+  | "JavaScript"
+  | "TypeScript"
+  | "Node.js"
+  | "Python"
+  | "Docker"
+  | "Kubernetes";
