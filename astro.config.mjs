891ed105a225fba94ab2ac86990899f696dd9681
asfalfa <glkin9@gmail.com>
2024-12-10 10:15:06 +0100

    feat: test adding node adapter for dockerization 3

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,7 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "static",
   site: "https://gomisroca.github.io",
   base: "portfolio",
   i18n: {
