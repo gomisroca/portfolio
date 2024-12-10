dd153091069438e027803274e62656704675c52e
asfalfa <glkin9@gmail.com>
2024-12-10 10:03:01 +0100

    feat: test adding node adapter for dockerization

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,14 @@ import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import icon from "astro-icon";
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "server",
+  adapter: node({
+    mode: "standalone",
+  }),
   site: "https://gomisroca.github.io",
   base: "portfolio",
   i18n: {
