4f5d5fb0b6d518d821dcb4c7b8a23fa27cee9bbf
asfalfa <glkin9@gmail.com>
2024-12-19 12:48:22 +0100

    feat: add course data to be used in about section

---
+++ src/lib/courses.ts
@@ -0,0 +1,72 @@
+const courses = [
+  {
+    name: "Python Programming",
+    date: "2022-2023",
+    school: "University of Helsinki",
+    certificates: [],
+    url: "https://programming-24.mooc.fi/",
+  },
+  {
+    name: "CS50's Introduction to Computer Science",
+    date: "2023-2024",
+    school: "HarvardX",
+    certificates: [
+      "https://certificates.cs50.io/c388235e-2e43-4ffe-a4b8-e14e2e3332f5",
+    ],
+    url: "https://cs50.harvard.edu/x/2024/",
+  },
+  {
+    name: "CS50's Introduction to Programming with Python",
+    date: "2023-2024",
+    school: "HarvardX",
+    certificates: [
+      "https://certificates.cs50.io/b877a105-8be3-4952-b173-2f64b89613ed",
+    ],
+    url: "https://cs50.harvard.edu/python/2022/",
+  },
+  {
+    name: "Full Stack Open",
+    date: "2023-2024",
+    school: "University of Helsinki",
+    certificates: [
+      "https://studies.cs.helsinki.fi/stats/api/certificate/fullstackopen/en/87e29f3f01e2b5c52030226e5574f2b7",
+      "https://studies.cs.helsinki.fi/stats/api/certificate/fs-graphql/en/205fae4c046ee5d583526ce548e2f7de",
+      "https://studies.cs.helsinki.fi/stats/api/certificate/fs-typescript/en/5a8dd91be416741de51300b05b199b24",
+      "https://studies.cs.helsinki.fi/stats/api/certificate/fs-react-native-2020/en/4cbf25d0309953e1475b8f2d4bf9fd78",
+      "https://studies.cs.helsinki.fi/stats/api/certificate/fs-cicd/en/f576dbddf8d6e9e2b1026d37db03c134",
+      "https://studies.cs.helsinki.fi/stats/api/certificate/fs-containers/en/ff522dd08c3409f8265f287e03def898",
+      "https://studies.cs.helsinki.fi/stats/api/certificate/fs-psql/en/4c7e59edf65fafa3a173a20e83a7a64b",
+    ],
+    url: "https://fullstackopen.com/en/",
+  },
+  {
+    name: "DevOps with Docker",
+    date: "2023-2024",
+    school: "University of Helsinki",
+    certificates: [],
+    url: "https://devopswithdocker.com/",
+  },
+  {
+    name: "DevOps with Kubernetes",
+    date: "2023-2024",
+    school: "University of Helsinki",
+    certificates: [],
+    url: "https://devopswithkubernetes.com/",
+  },
+  {
+    name: "Test-Driven Development",
+    date: "2024-",
+    school: "University of Helsinki",
+    certificates: [],
+    url: "https://tdd.mooc.fi/",
+  },
+  {
+    name: "Cyber Security Base",
+    date: "2024-",
+    school: "University of Helsinki",
+    certificates: [],
+    url: "https://cybersecuritybase.mooc.fi/",
+  },
+];
+
+export default courses;
