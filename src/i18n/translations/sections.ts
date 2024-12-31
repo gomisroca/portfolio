0bf2dd41df644f6630e383f35771a90e3db02fca
asfalfa <glkin9@gmail.com>
2024-12-31 16:03:35 +0100

    feat: add i18n text to card buttons

--- src/i18n/translations/sections.ts
+++ src/i18n/translations/sections.ts
@@ -2,18 +2,26 @@ const sectionsTranslations = {
   en: {
     projects: "Projects",
     retrospective: "Monthly Retrospective",
+    read: "Read",
+    explore: "Explore",
   },
   es: {
     projects: "Proyectos",
     retrospective: "Retrospectiva Mensual",
+    read: "Leer",
+    explore: "Explorar",
   },
   cat: {
     projects: "Projectes",
     retrospective: "Retrospectiva Mensual",
+    read: "Llegir",
+    explore: "Explorar",
   },
   de: {
     projects: "Projekte",
     retrospective: "Monatliche Retrospektive",
+    read: "Lesen",
+    explore: "Erkunden",
   },
 };
 
