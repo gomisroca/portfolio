61d9adf24e4a4e68e91f09a80d07037fa3df3610
asfalfa <glkin9@gmail.com>
2025-01-28 10:51:41 +0100

    fix: remove unused social media and make contact sheet more readable

--- src/i18n/translations/contact.ts
+++ src/i18n/translations/contact.ts
@@ -1,18 +1,18 @@
 const contactTranslations = {
   en: {
-    email: `You can contact me via email at <br/><a href="mailto:gomisroca@gmail.com" class="text-2xl font-semibold transition-colors duration-200 hover:text-primary-500">gomisroca@gmail.com</a>.`,
+    email: `You can contact me via email at <br/><a href="mailto:gomisroca@gmail.com" class="text-2xl font-semibold transition-colors duration-200 hover:text-primary-500">gomisroca@gmail.com</a>`,
     others: `You will also find me in the following platforms:`,
   },
   es: {
-    email: `Puedes contactar conmigo por correo electrónico en <br/><a href="mailto:gomisroca@gmail.com" class="text-2xl font-semibold transition-colors duration-200 hover:text-primary-500">gomisroca@gmail.com</a>.`,
+    email: `Puedes contactar conmigo por correo electrónico en <br/><a href="mailto:gomisroca@gmail.com" class="text-2xl font-semibold transition-colors duration-200 hover:text-primary-500">gomisroca@gmail.com</a>`,
     others: `También me encontrarás en las siguientes plataformas:`,
   },
   cat: {
-    email: `Pots contactar amb mi per correu electrònic a <br/><a href="mailto:gomisroca@gmail.com" class="text-2xl font-semibold transition-colors duration-200 hover:text-primary-500">gomisroca@gmail.com</a>.`,
+    email: `Pots contactar amb mi per correu electrònic a <br/><a href="mailto:gomisroca@gmail.com" class="text-2xl font-semibold transition-colors duration-200 hover:text-primary-500">gomisroca@gmail.com</a>`,
     others: `També em trobaràs a les següents plataformes:`,
   },
   de: {
-    email: `Sie können mich per E-Mail kontaktieren unter <br/><a href="mailto:gomisroca@gmail.com" class="text-2xl font-semibold transition-colors duration-200 hover:text-primary-500">gomisroca@gmail.com</a>.`,
+    email: `Sie können mich per E-Mail kontaktieren unter <br/><a href="mailto:gomisroca@gmail.com" class="text-2xl font-semibold transition-colors duration-200 hover:text-primary-500">gomisroca@gmail.com</a>`,
     others: `Sie finden mich auch auf folgenden Plattformen:`,
   },
 };
