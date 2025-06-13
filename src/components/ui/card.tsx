875b4d1dae1b5708083846dcee31a6ebf0b8d5a2
Adrià Gomis <gomisroca@gmail.com>
2025-06-13 13:12:30 +0200

    style: simplify styling logic

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -8,8 +8,8 @@ function Card({
   className?: string;
 }) {
   return (
-    <div className="group relative my-2 flex h-full w-full flex-col items-center justify-start gap-2 overflow-hidden transition-transform duration-200 ease-in-out hover:z-30 hover:scale-110 hover:shadow-md">
-      <div className="absolute inset-0 bg-gradient-to-r from-blue-400 via-emerald-400 to-lime-400 opacity-0 transition duration-200 ease-in-out group-hover:opacity-100 dark:from-blue-500 dark:via-purple-500 dark:to-pink-500"></div>
+    <div className="group relative my-2 flex h-full w-full flex-col items-center justify-start gap-2 overflow-hidden transition-transform duration-200 ease-in-out hover:z-30 hover:scale-110 hover:shadow-md active:scale-90 active:duration-100 xl:active:scale-100">
+      <div className="absolute top-0 h-[2px] w-full bg-gradient-to-r from-blue-400 via-emerald-400 to-lime-400 opacity-0 transition duration-200 ease-in-out group-hover:opacity-100 dark:from-blue-500 dark:via-purple-500 dark:to-pink-500"></div>
       <div
         className={twMerge(
           "relative z-20 m-[2px] flex h-full w-full flex-col items-center justify-start gap-2 bg-zinc-50 shadow-md group-hover:z-40 dark:bg-zinc-950 dark:shadow-white/10",
@@ -18,6 +18,7 @@ function Card({
       >
         {children}
       </div>
+      <div className="absolute bottom-0 h-[2px] w-full bg-gradient-to-r from-blue-400 via-emerald-400 to-lime-400 opacity-0 transition duration-200 ease-in-out group-hover:opacity-100 dark:from-blue-500 dark:via-purple-500 dark:to-pink-500"></div>
     </div>
   );
 }
--- src/components/ui/footer/FooterDock.tsx
+++ src/components/ui/footer/FooterDock.tsx
@@ -39,7 +39,7 @@ const LanguageDropdown = ({
         {activeLang.toUpperCase()}
       </li>
       {open && (
-        <ul className="absolute bottom-9 left-0 mx-auto flex w-fit flex-col items-start justify-start bg-zinc-200 p-1 dark:bg-zinc-900">
+        <ul className="absolute bottom-8 left-0 mx-auto flex w-fit flex-col items-start justify-start bg-zinc-50 p-1 dark:bg-zinc-950">
           {Object.entries(languages).map(([lang]) => (
             <li key={lang} className="px-2 py-1">
               <LanguageLink
@@ -78,7 +78,7 @@ function FooterDock({
   const [hovered, setHovered] = useState(false);
   return (
     <motion.ul
-      className="group fixed right-0 bottom-0 m-2 mx-auto h-fit w-fit bg-zinc-50 dark:bg-zinc-950"
+      className="group fixed right-0 bottom-0 left-0 m-2 mx-auto h-fit w-fit bg-zinc-50 dark:bg-zinc-950"
       initial="hidden"
       whileInView="visible"
       whileHover="hovered"
@@ -87,10 +87,14 @@ function FooterDock({
       onMouseEnter={() => setHovered(true)}
       onMouseLeave={() => setHovered(false)}
     >
-      <div className="absolute bottom-0 left-0 h-full w-[2px] bg-gradient-to-b from-blue-400 via-emerald-400 to-lime-400 dark:from-blue-500 dark:via-purple-500 dark:to-pink-500" />
+      <motion.hr
+        initial={{ scaleX: 0 }}
+        animate={{ scaleX: hovered ? 1 : 0 }}
+        transition={{ duration: hovered ? 0.5 : 0.2, ease: "linear" }}
+        className="absolute top-0 right-0 left-0 h-[2px] origin-bottom border-0 bg-gradient-to-r from-blue-400 via-emerald-400 to-lime-400 transition duration-200 ease-in-out dark:from-blue-500 dark:via-purple-500 dark:to-pink-500"
+      />
       <div className="flex flex-row space-x-2 bg-zinc-50 px-2 py-1 dark:bg-zinc-950">
         <LanguageDropdown activeLang={activeLang} path={path} />
-        <span className="m-auto h-4 border border-zinc-300 dark:border-zinc-700" />
         <motion.li
           className="w-8 text-center"
           variants={{
@@ -101,12 +105,7 @@ function FooterDock({
           <ThemeToggle />
         </motion.li>
       </div>
-      <motion.hr
-        initial={{ scaleX: 0 }}
-        animate={{ scaleX: hovered ? 1 : 0 }}
-        transition={{ duration: hovered ? 0.5 : 0.2, ease: "linear" }}
-        className="absolute right-0 bottom-0 left-0 h-[2px] origin-left border-0 bg-gradient-to-r from-blue-400 via-emerald-400 to-lime-400 transition duration-200 ease-in-out dark:from-blue-500 dark:via-purple-500 dark:to-pink-500"
-      />
+      <div className="absolute bottom-0 h-[2px] w-full origin-left bg-gradient-to-r from-blue-400 via-emerald-400 to-lime-400 dark:from-blue-500 dark:via-purple-500 dark:to-pink-500" />
     </motion.ul>
   );
 }
